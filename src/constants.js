f472142736df579c6cc83645fa395c00a8b92b60
trevor <me@trevorbehnke.com>
2021-11-06 13:11:16 -0700

    feat: :rocket: game config

---
+++ src/constants.js
@@ -0,0 +1,13 @@
+const CONTRACT_ADDRESS = "0x2a6F26239FEC0E80FcC62dd9963f85c31FE1c7Fb";
+
+const transformCharacterData = (characterData) => {
+  return {
+    name: characterData.name,
+    imageURI: characterData.imageURI,
+    hp: characterData.hp.toNumber(),
+    maxHp: characterData.maxHp.toNumber(),
+    attackDamage: characterData.attackDamage.toNumber(),
+  };
+};
+
+export { CONTRACT_ADDRESS, transformCharacterData };
